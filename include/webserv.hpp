6776cd34fa99f096fcb05d2a7e3109e1da757e5c
Wang Xue Rui <wangxuerui2003@gmail.com>
2024-01-20 12:54:20 +0800

    Fix linux compile and removing active connection

--- include/webserv.hpp
+++ include/webserv.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   webserv.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wxuerui <wxuerui@student.42.fr>            +#+  +:+       +#+        */
+/*   By: wxuerui <wangxuerui2003@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 16:01:20 by wxuerui           #+#    #+#             */
-/*   Updated: 2024/01/15 20:53:55 by wxuerui          ###   ########.fr       */
+/*   Updated: 2024/01/20 12:27:24 by wxuerui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,9 @@
 #define USAGE "Usage: ./webserv <config_file>"
 #define DEFAULT_CONFIG "./conf/test.conf"
 #define HTTP_REQUEST_TERMINATOR "\r\n\r\n"
+
+#ifdef __linux__
+	#include <errno.h>
+#endif
 
 #endif
