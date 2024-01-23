3c04f762bf96685076b7e082d069ab4de4ff3f7a
Wang Xue Rui <wangxuerui2003@gmail.com>
2024-01-23 21:07:54 +0800

    Add log util function (log to cout/cerr or log to file)

--- include/utils.hpp
+++ include/utils.hpp
@@ -6,17 +6,23 @@
 /*   By: wxuerui <wxuerui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 16:07:17 by wxuerui           #+#    #+#             */
-/*   Updated: 2024/01/15 20:42:12 by wxuerui          ###   ########.fr       */
+/*   Updated: 2024/01/23 21:07:03 by wxuerui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
+#include <iostream>
+#include <fstream>
+#include "colors.hpp"
+
 namespace wsutils {
 	void errorExit(const char *msg);
 	void warningOutput(const char *msg);
 	size_t convertSizeStringToBytes(const std::string& sizeString);
+	void log(std::string msg, std::ostream& is);
+	void log(std::string msg, std::string filename);
 }
 
 #endif
