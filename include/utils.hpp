19a0fae7d8603a3ba73923e748865094e5357b27
Wang Xue Rui <wangxuerui2003@gmail.com>
2024-01-24 10:09:27 +0800

    Change argument name from is to os for wsutils::log

--- include/utils.hpp
+++ include/utils.hpp
@@ -6,7 +6,7 @@
 /*   By: wxuerui <wxuerui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 16:07:17 by wxuerui           #+#    #+#             */
-/*   Updated: 2024/01/23 21:07:03 by wxuerui          ###   ########.fr       */
+/*   Updated: 2024/01/24 10:09:01 by wxuerui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ namespace wsutils {
 	void errorExit(const char *msg);
 	void warningOutput(const char *msg);
 	size_t convertSizeStringToBytes(const std::string& sizeString);
-	void log(std::string msg, std::ostream& is);
+	void log(std::string msg, std::ostream& os);
 	void log(std::string msg, std::string filename);
 }
 
