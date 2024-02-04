d4a4fb84ffa01b2219e82f4297691d713ff1eda5
Wang Xue Rui <wangxuerui2003@gmail.com>
2024-02-04 08:36:53 +0800

    chore: add todo for get_session.php

--- www/stateful_site/get_session.php
+++ www/stateful_site/get_session.php
@@ -1,3 +1,4 @@
 <?php
-// 
+// TODO: take a form of name and intra-id, send back to webserv with a Http header
+// TODO: webserv will then store the session data into a csv file and Set-Cookie for the client
 ?>
